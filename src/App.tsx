62e235017f4bd867d355192ea803229a9f6b95dd
avibeni01 <avibeni@hotmail.fr>
2025-04-02 10:08:04 +0300

    whatsap

--- src/App.tsx
+++ src/App.tsx
@@ -420,23 +420,19 @@ function App() {
 
       // Open WhatsApp
       setTimeout(() => {
-      const message = generateWhatsAppMessage();
-      const whatsappUrl = `https://wa.me/972584140489?text=${encodeURIComponent(message)}`;
-      console.log("URL WhatsApp:", whatsappUrl); // Vérifiez l'URL générée
-      const newWindow = window.open(whatsappUrl, '_blank');
-      if (!newWindow) {
-        console.error("Erreur: Impossible d'ouvrir la fenêtre WhatsApp.");
-      }
-      
-       // Créer un lien temporaire et le cliquer
-      const link = document.createElement('a');
+        const message = generateWhatsAppMessage();
+        const whatsappUrl = `https://wa.me/972584140489?text=${encodeURIComponent(message)}`;
+        console.log("URL WhatsApp:", whatsappUrl); // Vérifiez l'URL générée
+
+        // Créer un lien temporaire et le cliquer
+        const link = document.createElement('a');
         link.href = whatsappUrl;
         link.target = '_blank';
+        link.rel = 'noopener noreferrer'; // Ajout de rel pour des raisons de sécurité
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
       }, 300); // Délai d'une seconde pour laisser le temps au toast de s'afficher
-      
 
 
     } catch (error) {
